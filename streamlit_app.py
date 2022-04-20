acdee40ca282eea0591d5a34fdb34b0f195374fd
Martin Kingston <martinjkingston@gmail.com>
2022-04-20 12:18:39 +0100

    Updated app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
 
+streamlit.tile("My Mum's New Healthy Diner")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.title("y Parents New Health Diner")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
